6961c9ac7944f8796bf3b37151dff13b112c8b99
Cristian Reveco <crirev@gmail.com>
2021-01-14 19:20:59 -0300

    feat(admin):create sub-set component

---
+++ src/app/admin/components/sub-set/sub-set.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-set',
+  templateUrl: './sub-set.component.html',
+  styleUrls: ['./sub-set.component.less']
+})
+export class SubSetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
