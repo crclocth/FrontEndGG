d2ab6a394d6f0035d51ae95418453065c9b8740e
Cristian Reveco <crirev@gmail.com>
2021-01-14 19:22:00 -0300

    feat(admin):create admin routing

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
+import { GeneralComponent } from './screens/general/general.component';
+import { SectorComponent } from './components/sector/sector.component';
+import { SetComponent } from './components/set/set.component';
+import { SubSetComponent } from './components/sub-set/sub-set.component';
+import { StickersComponent } from './components/stickers/stickers.component';
+
+
+@NgModule({
+  declarations: [AdminComponent, GeneralComponent, SectorComponent, SetComponent, SubSetComponent, StickersComponent],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
