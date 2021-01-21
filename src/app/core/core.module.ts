d37cbdf8dbd1e3fd1dbcac2d228070f81ec88b9b
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-01-21 01:37:36 -0300

    feat(core): toatsr module fixed

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ToastrModule.forRoot({
+      progressBar: true,
+      closeButton: true,
+      positionClass: 'toast-top-right'
+    }),
   ]
 })
 export class CoreModule { }
