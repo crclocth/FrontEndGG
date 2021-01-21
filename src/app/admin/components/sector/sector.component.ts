d8170b463698ae438a67142e920b8b42a9b90d5c
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-01-20 23:28:13 -0300

    feat(admin): form group added

--- src/app/admin/components/sector/sector.component.ts
+++ src/app/admin/components/sector/sector.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sector',
@@ -6,10 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sector.component.less']
 })
 export class SectorComponent implements OnInit {
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  checkoutForm: FormGroup;
+  mensaje:string="";
+  isDivVisible = false;
+
+  constructor() {
+    this.checkoutForm = this.createFormGroup();
   }
 
+  ngOnInit() {
+  }
+  
+  createFormGroup() {
+    return new FormGroup({
+      nombreSector: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onSubmit(){
+    this.mensaje="Datos completados correctamente";
+    this.isDivVisible=true;
+  }
+
+  get nombreSector() { return this.checkoutForm.get('nombreSector')?.value;}
+
+  public postSector(){ 
+    let datosForm: string = this.checkoutForm.get('nombreSector')?.value;
+    console.log(datosForm);
+    try {
+      // this.sectorServices.poblarSectores(datosForm);
+      alert('Su sector se agrego correctamente');
+    } catch (error) {
+      alert('Su sector no pudo ser agregado');
+      
+    }
+  }
 }
