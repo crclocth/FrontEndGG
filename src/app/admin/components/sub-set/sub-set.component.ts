f64bd5087f9601c55085530984424c31c6bc9d9f
Cristian Reveco <crirev@gmail.com>
2021-01-21 01:39:20 -0300

    feat(admin):add forms

--- src/app/admin/components/sub-set/sub-set.component.ts
+++ src/app/admin/components/sub-set/sub-set.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sub-set',
@@ -8,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
 export class SubSetComponent implements OnInit {
 
   public opcion: number = 2;
-
-  constructor() { }
+  checkoutForm: FormGroup;
+  mensaje:string="";
+  isDivVisible = false;
+  constructor() {
+    this.checkoutForm = this.createFormGroup();
+   }
 
   ngOnInit(): void {
   }
@@ -17,6 +22,16 @@ export class SubSetComponent implements OnInit {
   setOptionn(num: number) {
     this.opcion = num;
   }
-
+  createFormGroup() {
+    return new FormGroup({
+      nombreperro: new FormControl('', [Validators.required]),
+      nombreduenno: new FormControl('', [Validators.required]),
+      razaa: new FormControl('',[Validators.required]),
+    });
+  }
+  onSubmit(){
+    this.mensaje="Datos completados correctamente";
+    this.isDivVisible=true;
+  }
 
 }
