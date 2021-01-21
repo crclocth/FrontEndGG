14833f11bb5baa998df8b4931609bea6dc6322a7
Cristian Reveco <crirev@gmail.com>
2021-01-21 11:24:10 -0300

    feat(admin): fix modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { ToastrModule } from 'ngx-toastr';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,11 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    CoreModule
+    CoreModule,
+    ToastrModule.forRoot(),
+    AdminModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
