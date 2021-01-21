00f35c84e713d98d9a85ccfa7b3eed5297f50f4a
Cristian Reveco <crirev@gmail.com>
2021-01-21 19:42:58 -0300

    feat(admin): import shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SubSectorComponent } from './sub-sector/sub-sector.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SubsetCardComponent } from './subset-card/subset-card.component';
 
 
 
 @NgModule({
-  declarations: [NavBarComponent, SubSectorComponent],
+  declarations: [NavBarComponent, SubSectorComponent, SubsetCardComponent,],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    
 
   ],
-  exports: [NavBarComponent, SubSectorComponent]
+  exports: [NavBarComponent, SubSectorComponent,SubsetCardComponent,]
 })
 export class SharedModule { }
