044035d0915e490a83eac204426e29a423147aaa
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-01-24 01:25:51 -0300

    feat(shared/navbar): kpi added

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Sector } from 'src/app/core/models/sector-model';
+import { SectorService } from 'src/app/core/services/sector/sector.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  public sectors: Sector[] = [];
+  public tamano: number;
+
+  constructor(
+    private sectorServices: SectorService
+  ) { 
+    this.sectors = this.getSectors();
+    this.tamano = this.getTamano();
+  }
 
   ngOnInit(): void {
+  }
+
+  getSectors(): Sector[]{
+    return this.sectorServices.getAllSectors();
+  }
+
+  getTamano(): number{
+    return this.sectorServices.getTamanio();
   }
 
 }
