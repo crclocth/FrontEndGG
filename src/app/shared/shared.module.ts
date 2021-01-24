096a6cb432f59d1208377f87e84de031b05bf7d0
Cristian Reveco <crirev@gmail.com>
2021-01-24 19:34:24 -0300

    feat(shared): add sticker module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,9 +5,10 @@ import { SubSectorComponent } from './sub-sector/sub-sector.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SubsetCardComponent } from './subset-card/subset-card.component';
 import { StickerCardComponent } from './sticker-card/sticker-card.component';
+import { StickerButtonComponent } from './sticker-button/sticker-button.component';
 
 const components = [
-  NavBarComponent, SubSectorComponent, SubsetCardComponent, StickerCardComponent
+  NavBarComponent, SubSectorComponent, SubsetCardComponent, StickerCardComponent,StickerButtonComponent,
 ]
 
 
