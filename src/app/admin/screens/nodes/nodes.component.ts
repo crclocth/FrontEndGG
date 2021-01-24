c52be96c4119939966c8da390c0096b455989594
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-01-24 20:25:49 -0300

    feat(nodes): functions fixed

--- src/app/admin/screens/nodes/nodes.component.ts
+++ src/app/admin/screens/nodes/nodes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Node } from 'src/app/core/models/node-model';
 import { NodeService } from 'src/app/core/services/node/node.service';
+import { NotificationService } from 'src/app/core/services/notification/notification.service';
 
 @Component({
   selector: 'app-nodes',
@@ -10,9 +11,11 @@ import { NodeService } from 'src/app/core/services/node/node.service';
 export class NodesComponent implements OnInit {
 
   public nodes: Node[] = [];
+  newNode: Node = null;
 
   constructor(
-    private nodeService: NodeService
+    private nodeService: NodeService,
+    private notificationService: NotificationService
   ) { 
     this.nodes = this.getNodes();
   }
@@ -22,6 +25,40 @@ export class NodesComponent implements OnInit {
 
   getNodes(): Node[]{
     return this.nodeService.getAllNodes();
+  }
+
+  public postNode(){ 
+    let datosForm: Node = {
+      nodeName: 'Nodo001',
+      nodeState: 'perfect',
+      sensors: [
+        {
+          sensorName: 'Sensor0001',
+          sensorState: 'perfect'
+        },
+        {
+          sensorName: 'Sensor0002',
+          sensorState: 'warning'
+        },
+        {
+          sensorName: 'Sensor0003',
+          sensorState: 'danger'
+        },
+        {
+          sensorName: 'Sensor0004',
+          sensorState: 'perfect'
+        }
+      ]
+    }
+    this.newNode = datosForm;
+    try {
+      if (this.nodeService.addNode(this.newNode) == null)
+        { this.notificationService.error('Ya existe este Sector!'); return;}
+      this.nodeService.addNode(this.newNode);
+      this.notificationService.success('Su sector ha sido agregado!');
+    } catch (error) {
+      this.notificationService.error('Error al agregar su sector');
+    }
   }
 
 }
