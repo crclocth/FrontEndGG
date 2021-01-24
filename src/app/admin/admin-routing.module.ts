16a57c641758e368cf1e196c598d14377c04219e
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-01-24 17:37:16 -0300

    feat(admin): routing fixed

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin.component';
 import { GeneralComponent } from './screens/general/general.component';
 import { NodesComponent } from './screens/nodes/nodes.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: GeneralComponent,
+    component: AdminComponent,
     children: [
       { path: 'general', component: GeneralComponent },
       { path: 'nodes', component: NodesComponent },
