2fa1144b88bb63b34b521ddbba0a374ab1777998
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-01-30 22:45:30 -0300

    feat(shared): providers integrated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,6 +8,7 @@ import { StickerCardComponent } from './sticker-card/sticker-card.component';
 import { StickerButtonComponent } from './sticker-button/sticker-button.component';
 import { ModalDeleteComponent } from './modal-delete/modal-delete.component';
 import { ModalDeleteSubComponent } from './modal-delete-sub/modal-delete-sub.component';
+import { NodeProviderService } from '../core/providers/node/node-provider.service';
 
 const components = [
   NavBarComponent, SubSectorComponent, SubsetCardComponent, StickerCardComponent,StickerButtonComponent,ModalDeleteComponent,ModalDeleteSubComponent
@@ -22,7 +23,7 @@ const components = [
     FormsModule,
     ReactiveFormsModule,
   ],
-
+  providers: [NodeProviderService],
   exports: [...components]
 
 })
