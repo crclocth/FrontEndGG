116d8163e196e7cb58c76d06135c9c545995b681
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-01-31 21:06:23 -0300

    feat(providers): providers fixed

--- src/app/core/providers/sensor/sensor-provider.service.ts
+++ src/app/core/providers/sensor/sensor-provider.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Sensor } from '../../models/sensor-model';
+import { HttpService } from '../../services/http/http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SensorProviderService {
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
+
+  public getAllUserNodeSensor(nodeId: string): Observable<Sensor[]> {
+    return this.httpService.get<Sensor[]>('/sensor/all/' + nodeId);
+  };
 }
