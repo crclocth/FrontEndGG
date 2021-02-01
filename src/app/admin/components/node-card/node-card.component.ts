b4cb0886408999d2cacfa9b7957c3cda457ba773
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2021-02-01 00:50:32 -0300

    feat(admin/nodeCard): accordion included

--- src/app/admin/components/node-card/node-card.component.ts
+++ src/app/admin/components/node-card/node-card.component.ts
@@ -13,10 +13,13 @@ export class NodeCardComponent implements OnInit {
 
   @Input() node: Node;
   public sensors$: Observable<Sensor[]>;
+  public sensor: Sensor;
   
   constructor(
     private sensorProvider: SensorProviderService
-  ) { }
+  ) { 
+    this.sensor = null;
+  }
 
   ngOnInit(): void {
     this.sensors$ = this.sensorProvider.getAllUserNodeSensor(this.node._id);
@@ -33,28 +36,28 @@ export class NodeCardComponent implements OnInit {
       case 'pressure':
         return 'Presión del Aire';
 
-      case 'pondtemperature':
+      case ('pondTemperature' || 'pondtemperature'):
         return 'Temperatura de Pozo';
 
-      case 'watertemperature':
+      case ('waterTemperature' || 'watertemperature'):
         return 'Temperatura del Agua';
 
-      case 'soiltemperature':
+      case ('soilTemperature' || 'soiltemperature'):
         return 'Temperatura de Suelo';
 
-      case 'soilhumidity':
+      case ('soilHumidity' || 'soilhumidity'):
         return 'Humedad de Suelo';
 
-      case 'waterlevel':
+      case ('waterLevel' || 'waterlevel'):
         return 'Nivel de Estanque';
 
       case 'current':
         return 'Corriente Eléctrica';
 
-      case 'waterflow':
+      case ('waterflow' || 'waterFlow'):
         return 'Caudal Agua';
 
-      case 'airflow':
+      case ('airflow' || 'airFlow'):
         return 'Caudal Viento';
 
       default:
